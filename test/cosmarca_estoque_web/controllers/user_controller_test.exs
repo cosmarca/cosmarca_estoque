a139fbb2c95eb42c108434e4a14050fc06afa4df
Gustavo Oliveira <oliveira.gustavo@totvs.com.br>
2019-03-27 19:02:34 -0300

    update tests connection

--- test/cosmarca_estoque_web/controllers/user_controller_test.exs
+++ test/cosmarca_estoque_web/controllers/user_controller_test.exs
@@ -27,7 +27,7 @@ defmodule CosmarcaEstoqueWeb.UserControllerTest do
   describe "index" do
     test "lists all users", %{conn: conn} do
       conn = get(conn, Routes.user_path(conn, :index))
-      assert html_response(conn, 200) =~ "Listing Users"
+      assert html_response(conn, 200) =~ "Lista de Usuários"
     end
   end
 
