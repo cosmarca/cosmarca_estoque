0f02b16de785c44eec31bc9b202bcffde8bc3e5f
Gustavo Oliveira <oliveira.gustavo@totvs.com.br>
2019-03-27 14:37:36 -0300

    user context

---
+++ lib/cosmarca_estoque/accounts/user.ex
@@ -0,0 +1,43 @@
+defmodule CosmarcaEstoque.Accounts.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "users" do
+    field :email, :string, unique: true
+    field :first_name, :string
+    field :last_name, :string
+    field :password_hash, :string
+    field :password, :string, virtual: true
+    field :password_confirmation, :string, virtual: true
+    field :role, :string, default: "user"
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:email, :first_name, :last_name, :password, :password_confirmation, :role])
+    |> validate_required([:email, 
+    :first_name, 
+    :last_name, 
+    :password,
+    :password_confirmation,
+     :role])
+     |> validate_format(:email, ~r/@/)
+     |> update_change(:email, &String.downcase(&1))
+     |> validate_length(:password, min: 6, max: 100)
+     |> validate_confirmation(:password)
+     |> unique_contraint(:email)
+     |> hash_password
+  end
+
+  defp hash_password(%Ecto.Changeset{valid?: true, changes: %{:password: :password}} = changeset) do
+    change(changeset, Comeonin.Argon2.add_hash(password))
+  end
+
+  defp hash_password(changeset) do
+    changeset
+  end
+
+end
---
+++ priv/repo/migrations/20190327173111_create_users.exs
@@ -0,0 +1,16 @@
+defmodule CosmarcaEstoque.Repo.Migrations.CreateUsers do
+  use Ecto.Migration
+
+  def change do
+    create table(:users) do
+      add :email, :string
+      add :first_name, :string
+      add :last_name, :string
+      add :password_hash, :string
+      add :role, :string
+
+      timestamps()
+    end
+
+  end
+end
