b87b12fdcdf081eed2db57b4c6f8ec12a6b33507
Gustavo Oliveira <oliveira.gustavo@totvs.com.br>
2019-03-29 07:42:44 -0300

    update secret

--- lib/cosmarca_estoque_web/controllers/page_controller.ex
+++ lib/cosmarca_estoque_web/controllers/page_controller.ex
@@ -2,8 +2,8 @@ defmodule CosmarcaEstoqueWeb.PageController do
   use CosmarcaEstoqueWeb, :controller
 
   def secret(conn, _) do
+    IO.inspect conn.private[:guardian_default_resource]
     user = Guardian.Plug.current_resource(conn)
-    IO.inspect user
     render(conn, "secret.html", current_user: user)
   end
   
