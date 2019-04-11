4bee0e975bcf4b526d07f47ad991f747f2147deb
Gustavo Oliveira <oliveira.gustavo@totvs.com.br>
2019-04-11 08:26:46 -0300

    update scheduler

--- config/config.exs
+++ config/config.exs
@@ -29,7 +29,7 @@ config :phoenix, :json_library, Jason
 config :cosmarca_estoque, CosmarcaEstoque.Scheduler,
   jobs: [
     phoenix_job: [
-      schedule: "*/15 * * * *",
+      schedule: "*/60 * * * *",
       task: {CosmarcaEstoque.Task, :work, []},
     ]
   ]
