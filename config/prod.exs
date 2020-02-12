559708344a7a4339c95cd6cbf7889798b31cd7a5
Gustavo Oliveira <gustavo.oliveira@hdi.com.br>
2020-02-12 14:47:51 -0300

    update domain

--- config/prod.exs
+++ config/prod.exs
@@ -12,7 +12,7 @@ use Mix.Config
 config :cosmarca_estoque, CosmarcaEstoqueWeb.Endpoint,
   load_from_system_env: true,
   http: [:inet6, port: System.get_env("PORT") || 4000],
-  url: [host: "app.cosmarca.com.br", port: 80],
+  url: [host: "log-zarah.me", port: 80],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   server: true,
